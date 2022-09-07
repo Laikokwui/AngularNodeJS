e21753d04326f0716ea016e3d39ae6507acf52f9
Laikokwei <kokwei325@gmail.com>
2022-09-07 15:31:30 +0800

    added security cors, morgan and helmet

--- node-rest-api/src/app.ts
+++ node-rest-api/src/app.ts
@@ -2,6 +2,9 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/customers";
 import * as mongoose from "mongoose";
+import * as helmet from "helmet";
+import * as cors from "cors";
+import * as morgan from "morgan";
 require('dotenv').config()
 
 class App {
@@ -18,6 +21,10 @@ class App {
     }
 
     private config(): void{
+        this.app.use(helmet.contentSecurityPolicy());
+        this.app.use(helmet.dnsPrefetchControl());
+        this.app.use(cors());
+        this.app.use(morgan('combined'));
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(function (req, res, next) {
